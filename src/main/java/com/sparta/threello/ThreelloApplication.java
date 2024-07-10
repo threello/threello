f98761a55dacf87f80ee62cffb428870ddbb0ddd
Dongseon <elwynn94@gmail.com>
2024-07-10 11:26:11 +0900

    Initial commit

---
+++ src/main/java/com/sparta/threello/ThreelloApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.threello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/threello/ThreelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.threello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
