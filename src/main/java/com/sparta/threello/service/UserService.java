e5a115b2dd28bac432d8b6e1999728788cd436ec
한근형 <165651118+hangeunhyeong@users.noreply.github.com>
2024-07-11 18:42:06 +0900

    [Conflict] conflict 해결

--- src/main/java/com/sparta/threello/service/UserService.java
+++ src/main/java/com/sparta/threello/service/UserService.java
@@ -4,16 +4,19 @@ import com.sparta.threello.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-    @Service
-    public class UserService {
+@Service
+public class UserService {
 
-        private final UserRepository userRepository;
-        private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-        @Autowired
-        public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-            this.userRepository = userRepository;
-            this.passwordEncoder = passwordEncoder;
-        }
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
 }
+
