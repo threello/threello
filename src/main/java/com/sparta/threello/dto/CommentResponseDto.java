40330259f99d7a1921d3d02ce77acbb5e9413fb1
jaewonchoi1234 <qwaszx9743@naver.com>
2024-07-12 12:19:11 +0900

    [ADD,Feat] Comment crud 추가

---
+++ src/main/java/com/sparta/threello/dto/CommentResponseDto.java
@@ -0,0 +1,24 @@
+package com.sparta.threello.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sparta.threello.entity.Comment;
+import java.time.LocalDateTime;
+
+
+public class CommentResponseDto {
+    private Long id;
+    private Long userId;
+    private String description;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime modifiedAt;
+
+    public CommentResponseDto(Comment comment) {
+        this.id = comment.getId();
+        this.userId = comment.getUser().getId();
+        this.description = comment.getDescription();
+        this.createdAt = comment.getCreatedAt();
+        this.modifiedAt = comment.getModifiedAt();
+    }
+}
